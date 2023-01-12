c847432afc750abb2876d05298d9fe89baabb90d
Pranay998 <63693691+Pranay998@users.noreply.github.com>
2023-01-12 10:54:01 +0530

    Add files via upload

---
+++ tollmanagement/src/main/java/com/project/tollmanagement/TollmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.project.tollmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TollmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TollmanagementApplication.class, args);
+	}
+
+}
